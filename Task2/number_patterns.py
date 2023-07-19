7b49a7cc53e3c0d4fae3b1ffb37b1661544e7b8c
rahulktd <rahulkpktd@gmail.com>
2023-07-19 11:04:44 +0530

    tuesday-update

--- Task2/number_patterns.py
+++ Task2/number_patterns.py
@@ -25,3 +25,32 @@ for i in range(1, n+1):
 print("\n")
 
 
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for k in range(i):
+        print(k,end=" ")
+    print()
+
+print('\n')
+
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(i):
+        print(k,end=" ")
+    print()
+
+print('\n')
+
+for i in range(1,n+1):
+    for j in range(n,i-1,-1):
+        print(j ,end=" ")
+    print()
+
+for i in range(n,0,-1):
+    x = i * (n-i)
+    y = i * ' '
+    print (int(x+y))
+
+
